7dd02870dd19621362122f94da1fb055218c7350
Zenos03 <xavier.evans2550@gmail.com>
2023-02-13 22:25:10 -0500

    Completes Project 02

---
+++ UI/HUD.gd
@@ -0,0 +1,19 @@
+extends Node
+
+func _ready():
+	update_score()
+	update_time()
+
+func update_score():
+	pass
+
+func update_time():
+	pass
+
+func update_lives():
+	pass
+
+func _on_Timer_timeout():
+	pass
+	
+		
